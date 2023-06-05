bfdf96fd3a37a3c18d316bec342ad3336680758e
Jaehwan Choi <lieblichoi@gmail.com>
2023-06-05 14:16:44 +0900

    Fix: 불필요 컴포넌트 정리

--- src/components/services/Router.tsx
+++ src/components/services/Router.tsx
@@ -2,18 +2,12 @@ import { Suspense } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import HomePage from '#pages/home';
-import CharacterPage from '#pages/character/[id]';
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <HomePage />,
-      errorElement: <div>error</div>,
-    },
-    {
-      path: '/character/:id',
-      element: <CharacterPage />,
       errorElement: <div>error</div>,
     },
   ]);
